7b78a096567651a091af6942177f1ae6d0767323
Ravin Gupta <ravingupta129@gmail.com>
2016-03-31 16:22:00 +0530

    custom_id added

--- routes/uuid_track.rb
+++ routes/uuid_track.rb
@@ -1,39 +1,63 @@
 module Dashboard
-	module UuidTrack
-		def self.registered(app)
-			
-			# Track UUID's
-			app.get '/uuid_track' do
-			  erb :track
-			end
-			# end route
+  module UuidTrack
+    def self.registered(app)
+      
+      # Track UUID's
+      app.get '/uuid_track' do
+        erb :track
+      end
+      # end route
 
-			# Get uuid details (to track uuids)
-			app.get '/uuid_details' do
-			  
-			  service_type = params['service_type']
-			  uuid_value = params['uuid_value'] # uuid_value = "bce946c6-e167-4b50-b145-1700eafa889b"
-			  response = []
-			  begin
-			    @cluster = Cassandra.cluster
-			    @sessionOcto = @cluster.connect(KEYSPACE)
-			    @selectUuidStatement = @sessionOcto.prepare(
-			      'SELECT * FROM octo.' + service_type + ' WHERE id=' + uuid_value
-			    )
-			    result = @sessionOcto.execute(@selectUuidStatement)
+      # Get uuid details (to track uuids)
+      app.get '/uuid_details' do
+        
+        service_type = params['service_type']
+        uuid_value = params['uuid_value'] # uuid_value = "bce946c6-e167-4b50-b145-1700eafa889b"
+        response = []
+        begin
 
-			    if result
-			      result.rows.each do |r|
-			        response.push(r.to_s)
-			      end
-			    end
-			  rescue Exception => e
-			    return "Error"
-			  end
-			return response
-			end
-			# end route
+          case service_type
+          when "app_init"
+            Octo::AppInit.where(customid: uuid_value).each do |r|
+              response.push(r.to_s)
+            end
+          when "app_login"
+            Octo::AppLogin.where(customid: uuid_value).each do |r|
+              response.push(r.to_s)
+            end
+          when "app_logout"
+            Octo::AppLogout.where(customid: uuid_value).each do |r|
+              response.push(r.to_s)
+            end
+          when "page_view"
+            Octo::Page.where(customid: uuid_value).each do |r|
+              response.push(r.to_s)
+            end
+          when "productpage_view"
+            Octo::Product.where(customid: uuid_value).each do |r|
+              response.push(r.to_s)
+            end
+          end
 
-		end
-	end
+          # @cluster = Cassandra.cluster
+          # @sessionOcto = @cluster.connect(KEYSPACE)
+          # @selectUuidStatement = @sessionOcto.prepare(
+          #   'SELECT * FROM octo.' + service_type + ' WHERE id=' + uuid_value
+          # )
+          # result = @sessionOcto.execute(@selectUuidStatement)
+
+          # if result
+          #   result.rows.each do |r|
+          #     response.push(r.to_s)
+          #   end
+          # end
+        rescue Exception => e
+          return "Error"
+        end
+      return response
+      end
+      # end route
+
+    end
+  end
 end
